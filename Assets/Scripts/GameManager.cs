ed024337ae1a9d19b4d22eb3d8a19e8218c75f3c
Dylan Ilvento <dylanilvento@gmail.com>
2021-02-07 12:34:48 -0500

    Added VSync change to fix screen tearing.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,7 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         Cursor.visible = false;
+        QualitySettings.vSyncCount = 1;
     }
 
     // Update is called once per frame
